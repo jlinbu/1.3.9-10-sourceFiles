620f65a192286b30b53bdfcb7f1289f7adb9f38f
GrayWolf24 <hel6505@comcast.net>
2017-03-24 13:55:48 -0400

    Add files via upload

--- team3.py
+++ team3.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'team' # Only 10 chars displayed.
+strategy_name = 'check'
+strategy_description = 'if other player deflected return deflect at later round. Play what the other player did two rounds before'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,8 +26,16 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    
+    return 'b'
+    if len(my_history)==2:
+        if their_history[-2]== 'b':
+            return 'b'
+        else:
+            if their_history[-2]=='c':
+                return 'c'
+    
+                    
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
