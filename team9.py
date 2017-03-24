09f31df73c58f9265622e536550364720718b79a
Ts14eagles <ts14eagles@gmail.com>
2017-03-24 14:03:42 -0400

    Add files via upload

--- team9.py
+++ team9.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'AmazonBase' # Only 10 chars displayed.
+strategy_name = 'Rush B'
+strategy_description = 'Start colluding, if betray then betray, if collude then collude with 40% chance of betray.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +18,16 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history)==0:
+        return 'c'
+    else:
+        if 'b' in their_history[-1]:
+            return 'b'
+        elif 'c' in their_history[-1]:
+            if random.random()<.4:
+                return 'b'
+            else:
+                return 'c'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
