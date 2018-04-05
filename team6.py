f094c5e930a0a0f3dd703f76f837a00adf464a2d
alexisharlacher <37903302+alexisharlacher@users.noreply.github.com>
2018-04-05 08:38:40 -0400

    Alexis

--- team6.py
+++ team6.py
@@ -1,32 +1,16 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+
 
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    primes = (2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,71,73,79,83,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,199)
+    for number in primes:
+        if len(their_history) % number == 0:
+            return "b"
+        else:
+            return "c"
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
