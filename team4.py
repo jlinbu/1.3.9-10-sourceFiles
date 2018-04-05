93b91d153f39cd7dffe3ef4f16c9d6dce2410dea
Nellayy <37903400+Nellayy@users.noreply.github.com>
2018-04-05 08:38:38 -0400

    Changedtomycode_NE

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Nellayy' # Only 10 chars displayed.
+strategy_name = 'The Switcheroo'
+strategy_description = 'This strategy is doing the opposite of what the their_history is. If a player decides to collude, my strategy would be betray. And if a player decides to betray, my strategy would be to collude.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if 'b' in their_history:
+        return 'c'
+    else: 
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
